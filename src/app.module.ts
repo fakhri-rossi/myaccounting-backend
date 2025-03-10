eeba2287a5eccec86c4a12cd66b963b24a382ed2
Fakhri Rossi <rossifakhri@gmail.com>
2025-03-10 11:39:17 +0700

    Revert "feat(auth): passport - implement local, jwt strategy; create signup endpoint"
    This reverts commit 7f02a7b5a353af4996740dc6964bcf49f8a9474c.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    AuthModule,
-    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
